6e338047a815678cc1b5d713ec7f4202a75b3e1e
HailuBoc <hailegebrelyalemberabate@outlook.com>
2023-03-05 09:15:00 +0300

    Add comment to main.js for clarity

--- client/main.js
+++ client/main.js
@@ -2,4 +2,5 @@ import React from 'react'
 import { hydrate } from 'react-dom'
 import App from './App'
 
+// Hydrate the React app for server-side rendering
 hydrate(<App/>, document.getElementById('root'))
