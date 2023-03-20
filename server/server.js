e256c8b87400a022fff3779df04cce9381c79858
HailuBoc <hailegebrelyalemberabate@outlook.com>
2023-03-20 16:45:00 +0300

    Add comments to server.js for better documentation

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ import config from './../config/config'
 import app from './express'
 import mongoose from 'mongoose'
 
-// Connection URL
+// Database connection
 mongoose.Promise = global.Promise
 mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
 
+// Start the server
 app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
