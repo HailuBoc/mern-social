4cc9bfac68ffe082d34880f33e2c68075f23edc0
HailuBoc <hailegebrelyalemberabate@outlook.com>
2023-10-15 11:00:00 +0300

    Format listByUser function with consistent spacing

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -46,14 +46,14 @@ const postByID = async (req, res, next, id) => {
 }
 
 const listByUser = async (req, res) => {
-  try{
+  try {
     let posts = await Post.find({postedBy: req.profile._id})
                           .populate('comments.postedBy', '_id name')
                           .populate('postedBy', '_id name')
                           .sort('-created')
                           .exec()
     res.json(posts)
-  }catch(err){
+  } catch (err) {
     return res.status(400).json({
       error: errorHandler.getErrorMessage(err)
     })
