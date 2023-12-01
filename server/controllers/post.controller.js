f3177a53cc2d55925dce57f45ffabd458a0fc5cd
HailuBoc <hailegebrelyalemberabate@outlook.com>
2023-12-01 10:00:00 +0300

    Format comment function with consistent spacing

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -119,13 +119,13 @@ const unlike = async (req, res) => {
 const comment = async (req, res) => {
   let comment = req.body.comment
   comment.postedBy = req.body.userId
-  try{
+  try {
     let result = await Post.findByIdAndUpdate(req.body.postId, {$push: {comments: comment}}, {new: true})
                             .populate('comments.postedBy', '_id name')
                             .populate('postedBy', '_id name')
                             .exec()
     res.json(result)
-  }catch(err){
+  } catch (err) {
     return res.status(400).json({
       error: errorHandler.getErrorMessage(err)
     })
